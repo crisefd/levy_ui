e79031fd80b92fa77d8ff1b2a607cfbdbded7875
Chris Fuertes <cristhian.fuertes@correounivalle.edu.co>
2019-06-02 11:06:48 -0500

    Creating levyService

--- src/assets/services/levyService.js
+++ src/assets/services/levyService.js
@@ -1,34 +1,19 @@
-import { Observable } from "rxjs";
 
 const baseUrl = "localhost:4000";
-export default {
+const apiVersion = "v1";
 
-    buildRequest = ([method, url, qParams, payload]) => {
-        const body = method === "GET" ? URLSearchParams(qParams) : JSON.stringify(payload);
-        const parameters = {
-            headers: { 'Content-Type': 'application/json' },
-            method: method,
-            mode: "cors",
-            body: body,
-            cache: "default"
-        };
-        return Observable.create((observer) => {
-            fetch(url, parameters)
-                .then(response => {
-                    observer.next(response.json());
-                    observer.complete();
-                })
-                .catch(error => {
-                    observer.error(error);
-                })
-        });
+import { request } from './apiRequester';
+
+export default levyService = {
+
+    signUp = (userData) => {
+        const url = `${baseUrl}/${apiVersion}/users/signup`;
+        return request('POST', url, null, userData);
     },
 
-    signUp = (email, password) => {
-        const url = `${baseUrl}/users/signup`;
-        return buildRequest([ 'POST', url,
-                               null,
-                              { email: email,
-                                password: password} ]);
+    signIn = (email, password) => {
+        const url = `${baseUrl}/${apiVersion}/users/signin`;
+        const data =  {email: email, password: password};
+        return request('POST', url, null, data);
     }
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+// import Rx from 'rxjs/Rx';
 import VueRx from "vue-rx";
 
 Vue.config.productionTip = false;
